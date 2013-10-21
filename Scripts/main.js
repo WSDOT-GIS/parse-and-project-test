60fbec668ec867448d8d97f73c443782c5adeee7
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-10-21 16:54:39 -0700

    Added map to page

---
+++ Scripts/main.js
@@ -0,0 +1,12 @@
+﻿require(["terraformer", "terraformer-wkt-parser", "esri/map"], function (Terraformer, TerraformerWKT, Map) {
+	"use strict";
+
+	var map;
+
+	map = new Map("map", {
+		basemap: "gray",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
+	});
+});
